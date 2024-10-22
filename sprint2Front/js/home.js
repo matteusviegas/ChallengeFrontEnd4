f73d75828bf6a956c2e17b5cd88b41d59f509681
rm561090@fiap.com.br <rm561090@fiap.com.br>
2024-10-22 16:49:41 -0300

    commit22/10

---
+++ sprint2Front/js/home.js
@@ -0,0 +1,4 @@
+//o plano daqui é mecher com os botoes, para aparecer no meio da tela para detalhar, como realmente um aviso msm, cheio de img,h1, e  "p"
+const dadoBotao = document.querySelectorAll('botao');
+
+dadoBotao.add
