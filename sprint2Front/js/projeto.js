9a471a1781f5e4a21ea8f06b281ac578735d4d76
ChallengeOne-MAT <rm561090@fiap.com.br>
2024-11-08 18:48:24 -0300

    ProjetoFutureStation_FrontEnd_FINALIZADO!  08/11/24

--- sprint2Front/js/projeto.js
+++ sprint2Front/js/projeto.js
@@ -98,24 +98,7 @@ document.querySelector('.form-pesquisa').addEventListener('submit', function (ev
     }
 });
 
-const box = document.querySelector(".container")
-const img = document.querySelectorAll(".container img")
-
-const active = document.querySelector('.infoOne')
-const elementh2 = document.querySelector('.info expandir')
 
 
-elementh2.addEventListener('click', ()=>{
-    elementh2.classList.toggle('active')
-})
-    let contador = 0 
-const slider =()=>{
-    contador++
-    if(contador > img.length -1){
-        contador =0;
-    }
-box.style.transform = `translateX(${-contador * 466}px)`
 
-}
 
-setInterval(slider, 2000);
