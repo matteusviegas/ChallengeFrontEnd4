bcab01a520d14d1ce7ca5f2baed6175eb91b9a16
matteusviegas <msmatteusbr2@gmail.com>
2025-02-25 11:57:50 -0300

    mexendo em Home

--- projetofrontendsprint3/src/app/page.tsx
+++ projetofrontendsprint3/src/app/page.tsx
@@ -7,54 +7,54 @@ export default function Home() {
 <main>
 
 <section>
-    <div>
-        <div className="w-1/2">
+    <div className="m-auto flex items-center border rounded-[12] bg-[#f9f9f9] w-[79%] p-4 "> 
+        <div className="w-[33%] ">
             <h1 className="ml-4  p-6 text-4xl" >Estações de Metrô</h1>
-            <img className="w-[400px] h-[250px]" src="Image/metro_linhaAmarela5.jpg" alt="metro1" />
+            <img className="w-[480px] h-[280px]" src="Image/metro_linhaAmarela5.jpg" alt="metro1" />
         </div>
 
-        <div>
-            <h3>Linha 4 Amarela</h3>
-            <p>Luz - Vila Sônia</p>
-            <p>04h AM às 00h.</p>
+        <div className="pl-14 pb-10 ">
+            <h3 className="  text-3xl ">Linha 4 Amarela</h3>
+            <p className=" text-2xl">Luz - Vila Sônia</p>
+            <p className=" text-2xl">04h AM às 00h.</p>
         </div>
     </div>
 
-    <div>
+    <div className="m-auto flex items-center border rounded-[12] bg-[#f9f9f9] w-[79%] h-[28px]p-4">
         <div>
             <h1 className="ml-4  p-6 text-4xl">Estações de Metrô</h1>
             <img className="w-[400px] h-[250px]" src="Image/imgmetro2.png" alt="metro1" />
 
         </div>
-        <div>
-            <h3>Linha 9 Esmeralda.</h3>
-            <p>Osasco - Grajaú.</p>
-            <p>04h AM às 00h.</p>
+        <div className="pl-14 pb-10">
+            <h3 className=" text-3xl ">Linha 9 Esmeralda.</h3>
+            <p className=" text-2xl ">Osasco - Grajaú.</p>
+            <p className=" text-2xl ">04h AM às 00h.</p>
         </div>
     </div>
     
-    <div>
+    <div className="m-auto flex items-center border rounded-[12] bg-[#f9f9f9] w-[79%] p-4">
         <div>
             <h1 className=" ml-4  p-6 text-4xl">Estações de Metrô</h1>
             <img className="w-[400px] h-[250px]" src="Image/linha_diamante.jpg" alt="metro3" />
 
         </div>
-        <div>
-            <h3>Linha 8 Diamante</h3>
-            <p>Júlio Prestes - Itapevi</p>
-            <p>04h AM às 00h.</p>
+        <div className="pl-14 pb-10">
+            <h3 className=" text-3xl ">Linha 8 Diamante</h3>
+            <p className=" text-2xl ">Júlio Prestes - Itapevi</p>
+            <p className=" text-2xl ">04h AM às 00h.</p>
         </div>
     </div>
 
 </section>
 
 <section>
-    <div className="bg-[#42807D] w-1/2 p-2.5">
+    <div className="bg-[#42807D] w-[80%] h-[500px] m-auto mt-10 p-2.5">
 
         <div className="flex items-center mb-4 ml-5">
             <div className="w-5 h-5 rounded-full bg-yellow-400 mr-7 mb-2"></div>
             <div>
-                <h2 className="text-white text-2xl m-0">LINHA 4 AMARELA</h2>
+                <h2 className="text-white mt-12 text-5xl m-0">LINHA 4 AMARELA</h2>
                 <p className="m-0 text-xl w-1/3">Operando normalmente</p>
             </div>
         </div>
