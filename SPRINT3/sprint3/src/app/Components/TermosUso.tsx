6d8e82c4f452de06aa1ad10c81bb90659544d270
Matteus viégas <160137962+matteusviegas@users.noreply.github.com>
2025-03-09 13:04:42 -0300

    adicionando termos de uso e logica de partida

--- SPRINT3/sprint3/src/app/Components/TermosUso.tsx
+++ SPRINT3/sprint3/src/app/Components/TermosUso.tsx
@@ -1,11 +1,51 @@
 const TermosUso = () => {
     return (
-      <div>
-        <h1>Termos de Uso</h1>
-        <p>Conteúdo dos termos de uso...</p>
-      </div>
+        <div>
+            <h1>Termos de Uso</h1>
+            <p>Estes Termos de Uso FutureStation regem o acesso e uso do aplicativo (FutureStation), desenvolvido e operado por Pedro, Sulamita e matteus, localizado em Vila mariana. Ao acessar ou usar o Aplicativo, você concorda em cumprir estes Termos. Caso não concorde com qualquer parte dos Termos, você não deve usar o Aplicativo.</p>
+            <h1>Aceitação dos Termos</h1>
+            <p>Ao acessar ou usar o Aplicativo, você concorda em estar vinculado por estes Termos e pela Política de Privacidade, que é incorporada a este documento por referência. Caso não concorde com esses Termos, não deverá usar o Aplicativo.</p>
+            <h1>Modificações dos Termos</h1>
+            <p>A FutureStation se reserva o direito de alterar, modificar ou atualizar estes Termos a qualquer momento, sem aviso prévio. As alterações serão publicadas neste documento e entrarão em vigor imediatamente após a publicação. Recomendamos que você consulte periodicamente esta página para verificar se houve modificações.
+
+            </p>
+            <h1>Cadastro e Conta de Usuário</h1>
+            <p>Para usar certos recursos do Aplicativo, pode ser necessário criar uma conta. Ao criar uma conta, você concorda em fornecer informações precisas, completas e atualizadas. Você é responsável por manter a confidencialidade de suas credenciais de login e por todas as atividades realizadas em sua conta. Caso perceba qualquer uso não autorizado de sua conta, você deve notificar imediatamente a FutureStation.</p>
+            <h1>Licença de Uso</h1>
+            A FutureStation concede a você uma licença limitada, não exclusiva e não transferível para acessar e usar o Aplicativo para fins pessoais e não comerciais, de acordo com estes Termos. Você não pode sublicenciar, distribuir, modificar, copiar ou criar trabalhos derivados do Aplicativo sem a permissão expressa da FutureStation.
+            <h1>Propriedade Intelectual</h1>
+            <p>Todos os direitos de propriedade intelectual relacionados ao Aplicativo, incluindo, mas não se limitando a, direitos autorais, marcas registradas, patentes e segredos comerciais, pertencem exclusivamente à FutureStaion. Você concorda em não infringir esses direitos.
+
+            </p>
+            <h1> Proibição de Uso Indevido</h1>
+            <p>Você concorda em não usar o Aplicativo para:</p>
+          
+            <div>
+            <ul>
+                <li>Violar qualquer lei ou regulamento aplicável.
+                </li>
+                <li>Enviar ou transmitir vírus, malware ou outro conteúdo prejudicial.
+                </li>
+                <li>Interferir ou prejudicar o funcionamento do Aplicativo ou de seus servidores.
+                </li>
+                <li> Realizar qualquer atividade que possa causar danos, interrupções ou sobrecarga no Aplicativo.
+                </li>
+                <li>Usar o Aplicativo de maneira fraudulenta ou em violação de qualquer direito de propriedade intelectual de terceiros.
+                </li>
+
+            </ul>
+            </div>
+
+            <h1> Foro e Legislação Aplicável</h1>
+            <p>Este Termo será regido e interpretado de acordo com as leis do São Paulo. Qualquer disputa que surja em relação a estes Termos será resolvida exclusivamente pelos tribunais competentes da cidade de São Paulo, São Paulo.
+
+</p>
+<h1> Disposições Gerais</h1>
+        <p>Se qualquer disposição destes Termos for considerada inválida ou inaplicável, as disposições restantes permanecerão em pleno vigor. A falha da FutureStation sem exercer qualquer direito ou disposição destes Termos não constituirá renúncia a esse direito.
+
+</p>
+        </div>
     );
-  };
-  
-  export default TermosUso;
-  
+};
+
+export default TermosUso;
