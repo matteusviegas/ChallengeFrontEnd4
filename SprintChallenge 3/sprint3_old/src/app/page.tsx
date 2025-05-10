d40da69b362f164e6bd64ae5c6fe1561ffb01673
Matteus Viegas <msmatteusbr2@gmail.com>
2025-05-09 21:01:00 -0300

    reestruturando cadastro/login

--- SprintChallenge 3/sprint3_old/src/app/page.tsx
+++ SprintChallenge 3/sprint3_old/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
     
 <TelaLogin/>
- <div className="bg-white">
+ <div className="bg-wshite">
   <button className="mx-auto bg-[#42807D] mt-6 w-[15%] p-3  text-white rounded-2xl font-bold text-1xl">Atualizar</button>
  </div>
     </div>
