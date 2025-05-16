8aee2c58ebe626ccb647b812c57c7d77fdc660be
matteusviegas <msmatteusbr2@gmail.com>
2025-05-16 16:19:57 -0300

    mudanças para o vercel

--- SprintChallenge 3/sprint3_old/src/app/page.tsx
+++ SprintChallenge 3/sprint3_old/src/app/page.tsx
@@ -5,10 +5,9 @@ export default function Home() {
   return (
     <>
       <div className="text-center">
-        <Header />
         <div className="bg-white">
           <button className="mx-auto bg-[#42807D] mt-6 w-[15%] p-3 text-white rounded-2xl font-bold text-1xl">
-            Atualizar
+            atualizar
           </button>
           <TelaLogin /> 
         </div>
